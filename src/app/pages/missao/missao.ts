58ee2d648e94b0cde08ace7ad0a39864cda05133
Tallys Labanca <tallyslabanca20@gmail.com>
2026-03-17 11:06:15 -0300

    Restaurando pastas

---
+++ src/app/pages/missao/missao.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-missao',
+  imports: [],
+  templateUrl: './missao.html',
+  styleUrl: './missao.scss',
+})
+export class Missao {
+
+}
