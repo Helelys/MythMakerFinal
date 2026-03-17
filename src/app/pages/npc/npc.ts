4d9d50ca996aaf6eb733e2406333c163f7787718
Tallys Labanca <tallyslabanca20@gmail.com>
2026-03-17 11:07:21 -0300

    corrigindo bugs

--- src/app/pages/npc/npc.ts
+++ src/app/pages/npc/npc.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { NpcGenerator } from "../../components/npc-generator/npc-generator";
-import { Header } from "../../components/header/header";
 
 @Component({
   selector: 'app-npc',
-  imports: [NpcGenerator, Header],
+  imports: [NpcGenerator],
   templateUrl: './npc.html',
   styleUrl: './npc.scss',
 })
