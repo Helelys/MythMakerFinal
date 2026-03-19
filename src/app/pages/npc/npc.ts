453144a63d772a3a07cf6f445332530ede87a41e
Tallys Labanca <tallyslabanca20@gmail.com>
2026-03-19 17:12:13 -0300

    Feature: Subindo o NPC Generator de DND

--- src/app/pages/npc/npc.ts
+++ src/app/pages/npc/npc.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NpcGenerator } from "../../components/npc-generator/npc-generator";
+import { NpcGeradorDnd } from '../../components/npc-gerador-dnd/npc-gerador-dnd';
+
 
 @Component({
   selector: 'app-npc',
-  imports: [NpcGenerator],
+  imports: [NpcGenerator, NpcGeradorDnd],
   templateUrl: './npc.html',
   styleUrl: './npc.scss',
 })
