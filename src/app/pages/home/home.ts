214036979b5e9fae057b49c31c9903d2a9ba8a06
Tallys Labanca <tallyslabanca20@gmail.com>
2026-03-19 23:01:18 -0300

    Feature: Hero Banner

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeroBanner } from "../../components/hero-banner/hero-banner";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeroBanner],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
